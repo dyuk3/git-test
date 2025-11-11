cade313c245dbc33b0ff05cd061b5b968e18db8c
unknown <vaibhavsinghyo@gmail.com>
2025-11-11 12:09:10 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import qrcode from './qrcode.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='qrCard'>
+        <div className='qr'>
+          <img className='qrImg' src={qrcode} alt='' />
+        </div>
+        <div className='cardInfo'>
+          <h2>Improve your front-end skills by building projects</h2>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
